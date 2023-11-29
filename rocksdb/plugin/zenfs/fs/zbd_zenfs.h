e8328a7b7afaaa7f78d475bd9c2ea861beb2cbbb
gs201 <sg20180546@gmail.com>
2023-11-29 21:31:43 +0900

    numlevelfilelist

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.h
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.h
@@ -537,7 +537,7 @@ class ZonedBlockDevice {
     }
     void PrintStat(void){
       //   Sec    | Free |  RC |  RCZ |  RCP  | R_wp  |      Twp   |   erase_sz   |      erase_sz_zc |   p_er_sz      |
-      printf("[%4d] | %3ld  | %3ld |  %3ld | [%3ld] | [ %4ld ] | [ %3ld ] | [ %10ld ] | [ %10ld ] | [ %10ld ] |", 
+      printf("[%4d] | %3ld  | %3ld |  %3ld | [%3ld] | [ %4ld] | [ %3ld ] | [ %10ld ] | [ %10ld ] | [ %10ld ] |", 
                 T_, free_percent_, reset_count_,reset_count_zc_,partial_reset_count_,
              R_wp_, (RT_ >> 20),(erase_size_>>20),(erase_size_zc_>>20),(partial_erase_size_>>20));
       for(int n : num_files_levels_){
