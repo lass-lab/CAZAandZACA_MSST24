4f5421edc3f1d635178827a33f0d53d1c23271b8
gs201 <sg20180546@gmail.com>
2023-12-05 21:14:58 +0900

    Inputinzonelogic

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.h
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.h
@@ -1034,7 +1034,8 @@ class ZonedBlockDevice {
 
 
   IOStatus AllocateCompactionAwaredZone(Slice& smallest, Slice& largest ,int level, 
-                                          Env::WriteLifeTimeHint file_lifetime ,Zone **zone_out, std::vector<uint64_t> input_fno
+                                          Env::WriteLifeTimeHint file_lifetime , std::vector<uint64_t> input_fno,
+                                          Zone **zone_out,
                                           uint64_t min_capacity = 0);
   
 
