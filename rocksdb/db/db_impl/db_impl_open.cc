997057ba53247e96906d9b850f9361090d3b09a5
gs201 <sg20180546@gmail.com>
2023-12-07 20:24:50 +0900

    writeoptionDisableWaltrue

--- rocksdb/db/db_impl/db_impl_open.cc
+++ rocksdb/db/db_impl/db_impl_open.cc
@@ -1609,6 +1609,7 @@ Status DB::Open(const DBOptions& db_options, const std::string& dbname,
                 std::vector<ColumnFamilyHandle*>* handles, DB** dbptr) {
   const bool kSeqPerBatch = true;
   const bool kBatchPerTxn = true;
+  printf("DB::Open %p\n",db_options.env->GetFileSystem().get());
   return DBImpl::Open(db_options, dbname, column_families, handles, dbptr,
                       !kSeqPerBatch, kBatchPerTxn);
 }
