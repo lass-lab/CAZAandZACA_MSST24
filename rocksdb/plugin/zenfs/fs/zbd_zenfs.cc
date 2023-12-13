ccb807dffb08ca3dc5d0ca30e73803d13f430de6
gs201 <sg20180546@gmail.com>
2023-12-13 20:28:05 +0900

    avginvalidratiotimelapse

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
@@ -1080,12 +1080,11 @@ void ZonedBlockDevice::AddTimeLapse(int T) {
   for(auto z : io_zones){
     uint64_t invalid_size = (z->wp_ -z->start_) - z->used_capacity_;
 
-    if(z->wp_ -z->start_){
-      ratio = (invalid_size/(z->wp_ -z->start_));
+    if(z->wp_ - z->start_){
+      ratio = (double)(invalid_size/(double)(z-)>wp_ -z->start_));
     }else{
       ratio = 0.0;
     }
-    
     ratio_sum+=ratio;
   }
   double avg_invalid_ratio = ratio_sum/(io_zones.size());
