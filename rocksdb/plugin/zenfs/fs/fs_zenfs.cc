3d38885baff91bd7de7a474588628a5e8d54ef74
gs201 <sg20180546@gmail.com>
2023-12-15 11:14:39 +0900

    noprintf

--- rocksdb/plugin/zenfs/fs/fs_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/fs_zenfs.cc
@@ -1239,10 +1239,10 @@ void ZenFS::SetResetScheme(uint32_t r,uint32_t partial_reset_scheme,uint64_t T,u
 
 double ZenFS::GetMaxInvalidateCompactionScore(std::vector<uint64_t>& file_candidates,uint64_t * candidate_size) {
   // return file_candidates.size();
-  double ret = zbd_->GetMaxSameZoneScore(file_candidates);
+  // double ret = zbd_->GetMaxSameZoneScore(file_candidates);
   // printf()
-  return ret;
-  (void)(candidate_size);
+  // return ret;
+  // (void)(candidate_size);
   return zbd_->GetMaxInvalidateCompactionScore(file_candidates,candidate_size,false);
 }
 
