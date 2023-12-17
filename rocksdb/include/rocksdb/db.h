2fbe16cebd4b420b6b01ff3ee58593caa6c1387b
gs201 <sg20180546@gmail.com>
2023-12-17 14:53:15 +0900

    ReCalculateL0CompactionScore

--- rocksdb/include/rocksdb/db.h
+++ rocksdb/include/rocksdb/db.h
@@ -387,7 +387,7 @@ class DB {
   virtual double ReCalculateCompactionScore(int level);
   virtual std::vector<int> NumLevelsFiles(void);
   virtual std::vector<double> LevelsCompactionScore(void);
-  virtual double GetL0CompactionScore(void);
+  // virtual double GetL0CompactionScore(void);
   virtual std::vector<uint64_t> LevelsSize(void);
   virtual const Comparator* GetDefaultICMP(void);
   
