2f09aadbbf7b0d225df77f47d687a7636598e97c
gs201 <sg20180546@gmail.com>
2023-12-22 19:39:46 +0900

    GETFREESPACE

--- rocksdb/db/column_family.cc
+++ rocksdb/db/column_family.cc
@@ -883,10 +883,10 @@ ColumnFamilyData::GetWriteStallConditionAndCause(
     const MutableCFOptions& mutable_cf_options,
     const ImmutableCFOptions& immutable_cf_options) {
   // fs_
-  // uint64_t zns_free_space;
-  // uint64_t zns_free_percent;
+  uint64_t zns_free_space;
+  uint64_t zns_free_percent;
   
-  // ioptions()->fs->GetFreeSpace(std::string(),IOOptions(),&zns_free_space,&zns_free_percent,nullptr);
+  ioptions()->fs->GetFreeSpace(std::string(),IOOptions(),&zns_free_space,&zns_free_percent,nullptr);
   // printf("%lu %lu\n",zns_free_space,zns_free_percent);
   // if(zns_free_percent<=5){
   //   return {WriteStallCondition::kStopped,WriteStallCause::kNoFreeSpaceInZNS};
