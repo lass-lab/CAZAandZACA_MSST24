2d35e5d88e6ca50f82c30b6363ce0aefe8ec99ca
gs201 <sg20180546@gmail.com>
2024-01-24 16:28:30 +0900

    GetDefaultExtentSize

--- rocksdb/plugin/zenfs/fs/io_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/io_zenfs.cc
@@ -1136,7 +1136,7 @@ ZonedWritableFile::ZonedWritableFile(ZonedBlockDevice* zbd, bool _buffered,
   sparse_buffer = nullptr;
   buffer = nullptr;
 
-  uint64_t default_buffer_size = zbd_->GetDefaultExtentSize();
+  uint64_t default_buffer_size = zbd->GetDefaultExtentSize();
 
   if (buffered) {
     if (zoneFile->IsSparse()) {
