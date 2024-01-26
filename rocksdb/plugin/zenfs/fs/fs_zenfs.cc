575e5fd6a828bda6fe7a43b21e7730b93d454a2e
gs201 <sg20180546@gmail.com>
2024-01-26 15:43:38 +0900

    takemigratezonereleaseatfinal

--- rocksdb/plugin/zenfs/fs/fs_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/fs_zenfs.cc
@@ -3288,7 +3288,7 @@ IOStatus ZenFS::AsyncMigrateFileExtentsWorker(
     // io_uring_queue_exit(read_ring);
     return IOStatus::OK();
   }
-  Zone* target_zone=nullptr;
+
 
   while(read_reaped_n < extent_n){
     struct io_uring_cqe* cqe = nullptr;
@@ -3327,20 +3327,12 @@ IOStatus ZenFS::AsyncMigrateFileExtentsWorker(
       Info(logger_, "Migrate extent not found, ext_start: %lu", reaped_read_iocb->start_);
       continue;
     }
-
+    Zone* target_zone=nullptr;
     ZoneExtent* cur_ext = (*it);
-    if(target_zone==nullptr){
-      s = zbd_->TakeMigrateZone(zfile->smallest_,zfile->largest_,zfile->level_, &target_zone, zfile->GetWriteLifeTimeHint(),
-                                  zfile->predicted_size_,
-                                  cur_ext->length_,&run_gc_worker_,zfile->IsSST());
-    }
-    if(target_zone && target_zone->capacity_<cur_ext->length_){
-      zbd_->ReleaseMigrateZone(target_zone);
-      s = zbd_->TakeMigrateZone(zfile->smallest_,zfile->largest_,zfile->level_, &target_zone, zfile->GetWriteLifeTimeHint(),
-                                  zfile->predicted_size_,
-                                  cur_ext->length_,&run_gc_worker_,zfile->IsSST());
-    }
 
+    s = zbd_->TakeMigrateZone(zfile->smallest_,zfile->largest_,zfile->level_, &target_zone, zfile->GetWriteLifeTimeHint(),
+                                zfile->predicted_size_,
+                                cur_ext->length_,&run_gc_worker_,zfile->IsSST());
   
     if(!run_gc_worker_){
       // for(size_t a = 0 ;a < to_be_freed.size();a++){
@@ -3375,7 +3367,7 @@ IOStatus ZenFS::AsyncMigrateFileExtentsWorker(
       zfile->ReleaseWRLock();
       break;
     }
-    // zbd_->ReleaseMigrateZone(target_zone);
+    zbd_->ReleaseMigrateZone(target_zone);
 
 
   }
@@ -3425,7 +3417,6 @@ IOStatus ZenFS::AsyncMigrateFileExtentsWorker(
   // }
 
   // clock_gettime(CLOCK_MONOTONIC, &start_timespec);
-  zbd_->ReleaseMigrateZone(target_zone);
   zbd_->AddGCBytesWritten(copied);
   SyncFileExtents(zfile.get(), new_extent_list);
   zfile->ReleaseWRLock();
