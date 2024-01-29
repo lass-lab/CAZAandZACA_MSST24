2e30eb391793812a74cc0801f127576656b61110
gs201 <sg20180546@gmail.com>
2024-01-30 00:16:26 +0900

    CompactionAwareZoneCleaning

--- rocksdb/plugin/zenfs/fs/fs_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/fs_zenfs.cc
@@ -3306,7 +3306,7 @@ void ZenFS::BackgroundAsyncStructureCleaner(void){
       clock_gettime(CLOCK_MONOTONIC, &end_timespec);
       uint64_t after = zbd_->GetGCBytesWritten();
       long elapsed_ns_timespec = (end_timespec.tv_sec - start_timespec.tv_sec) * 1000000000 + (end_timespec.tv_nsec - start_timespec.tv_nsec);
-      printf("\t\t\t\t\t %lu\t(ms)\t%lu\t(MB)\t\t%d\n", (elapsed_ns_timespec/1000)/1000,(after-prev)>>20,mount_time_.load() );
+      printf("\t\t\t\t\t %lu\t(ms)\t%lu\t(MB)\t\t%d\t%lu\n", (elapsed_ns_timespec/1000)/1000,(after-prev)>>20,mount_time_.load(),cur_ops_ );
     }
 
   }
