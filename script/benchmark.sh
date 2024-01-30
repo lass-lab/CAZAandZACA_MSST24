b6c8893beb928b7f63a7f8359e5f1f86f5d597eb
gs201 <sg20180546@gmail.com>
2024-01-31 01:49:03 +0900

    SortByLBA

--- script/benchmark.sh
+++ script/benchmark.sh
@@ -1,8 +1,8 @@
 ## PATH
-ROCKSDB_PATH=/home/femu/CAZA/rocksdb
-RESULT_DIR_ROOT_PATH=/home/femu/access_testdata
+ROCKSDB_PATH=/home/sungjin/ZC_SMR/rocksdb
+RESULT_DIR_ROOT_PATH=/home/sungjin/access_testdata
 LOG_PATH=~/log
-RAW_ZNS=nvme0n1
+RAW_ZNS=sdb
 RAW_ZNS_PATH=/sys/block/${RAW_ZNS}/queue/scheduler
 
 
@@ -55,15 +55,15 @@ HEAVY=$G72
 # 36gb 37748736
 ## Tuning Point
 T=100
-T_COMPACTION=4
+T_COMPACTION=1
 T_SUBCOMPACTION=8
-T_FLUSH=8
+T_FLUSH=1
 ZC_KICKS=20
 UNTIL=20
 
 # SLOWDOWN_TRIGGER=16
 # STOPS_TRIGGER=16
-SIZE=$HEAVY
+SIZE=$G48
 # SIZE=83047219
 # MOTIV_SMALL_ME_256MB_ERASEBLOCK_64MB
 # FAR_LARGE_ME_256MB_ERASEBLOCK_64MB3
@@ -82,23 +82,28 @@ MAX_COMPACTION_START_LEVEL=2
 
 INPUT_AWARE_SCHEME=0
 
+MB256=268435456
+MB1=1048576
+
+READ_THEN_WRITE=0
+WRITE_AFTER_READ_DONE=1
+
+
 
 while :
 do
     FAILED=0
-    # for ALLOCATION_ALGORITHM in $RUNTIME_ZONE_RESET_ONLY $PARTIAL_RESET_WITH_ZONE_RESET
-    for ALLOCATION_ALGORITHM in  $CAZA_ADV
+    for ZC_SCHEME in  $READ_THEN_WRITE $WRITE_AFTER_READ_DONE
     do
-        for i in 30 40 50 60 70 100
+        for i in 1
         do
-        if [ $ALLOCATION_ALGORITHM -eq $LIZA ]; then
-            RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/LIZA
-            MAX_COMPACTION_KICK=27
-        elif [ $ALLOCATION_ALGORITHM -eq $CAZA ]; then
-            RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/CAZA
-        elif [ $ALLOCATION_ALGORITHM -eq $CAZA_ADV ]; then
-            RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/CAZA_ADV
-            MAX_COMPACTION_KICK=22
+        if [ $ZC_SCHEME -eq $READ_THEN_WRITE ]; then
+            RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/READ_THEN_WRITE
+        elif [ $ZC_SCHEME -eq $WRITE_AFTER_READ_DONE ]; then
+            RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/WRITE_AFTER_READ_DONE
+        # elif [ $ALLOCATION_ALGORITHM -eq $CAZA_ADV ]; then
+        #     RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/CAZA_ADV
+        #     MAX_COMPACTION_KICK=22
         # elif [ $ALLOCATION_ALGORITHM -eq $RUNTIME_ZONE_RESET_ONLY ]; then
         #     RESULT_DIR_PATH=${RESULT_DIR_ROOT_PATH}/RUNTIME_ZONE_RESET_ONLY
         # elif [ $ALLOCATION_ALGORITHM -eq $PROACTIVE_ZC ]; then
@@ -115,7 +120,7 @@ do
             echo "NO ${RESULT_DIR_PATH}"
             mkdir ${RESULT_DIR_PATH}
         fi
-            for COMPACTION_ALGORITHM in $MAX_INVALIDATION_COMPACTION
+            for IO_SIZE in $MB256 $MB1
                 do
                     if [ $COMPACTION_ALGORITHM -eq $BASELINE_COMPACTION ]; then
                         RESULT_PATH=${RESULT_DIR_PATH}/result_${SIZE}_BASELINE_FAR_${i}.txt
@@ -155,15 +160,15 @@ do
                         sleep 3
                         echo $RESULT_PATH
                         sudo ${ROCKSDB_PATH}/db_bench \
-                         -num=${SIZE} -benchmarks="fillrandom,stats" --fs_uri=zenfs://dev:nvme0n1 -statistics  -value_size=1024 \
+                         -num=${SIZE} -benchmarks="fillrandom,stats" --fs_uri=zenfs://dev:sdb -statistics  -value_size=1024 \
                           -max_background_compactions=${T_COMPACTION}   -max_background_flushes=${T_FLUSH} -subcompactions=${T_SUBCOMPACTION}  \
                           -histogram -seed=1699101730035899  -wait_for_compactions=false -enable_intraL0_compaction=false \
-                        -reset_scheme=0 -tuning_point=100 -partial_reset_scheme=1 -disable_wal=true -zc=${ZC_KICKS} -until=${UNTIL} \
-                        -allocation_scheme=${ALLOCATION_ALGORITHM}  -compaction_scheme=${COMPACTION_ALGORITHM} \
-                         -max_compaction_start_level=${MAX_COMPACTION_START_LEVEL} -input_aware_scheme=${MAX_INVALIDATION_COMPACTION}  \
-                        -max_compaction_kick=${i} > ${RESULT_DIR_PATH}/tmp
+                        -reset_scheme=0 -tuning_point=100 -partial_reset_scheme=1 -disable_wal=false -zc=${ZC_KICKS} -until=${UNTIL} \
+                        -allocation_scheme=0  -compaction_scheme=0 \
+                         -max_compaction_start_level=5 -input_aware_scheme=0 \
+                        -max_compaction_kick=0 -default_extent_size=${IO_SIZE} -async_zc_enabled=${ZC_SCHEME} > ${RESULT_DIR_PATH}/tmp
 
-                        # -num=${SIZE} -benchmarks="fillrandom,stats" --fs_uri=zenfs://dev:nvme0n1 -statistics  -value_size=1024 \
+                        # -num=${SIZE} -benchmarks="fillrandom,stats" --fs_uri=zenfs://dev:sdb -statistics  -value_size=1024 \
                         #   -max_background_compactions=${T_COMPACTION}   -max_background_flushes=${T_FLUSH} -subcompactions=${T_SUBCOMPACTION}  \
                         #   -histogram -seed=1699101730035899  -wait_for_compactions=false -enable_intraL0_compaction=false \
                         # -reset_scheme=0 -tuning_point=100 -partial_reset_scheme=1 -disable_wal=true -zc=${ZC_KICKS} -until=${UNTIL} \
@@ -196,8 +201,8 @@ do
             sleep 10
         done
 #        sleep 10
-#        sudo /home/femu/dummy 1 1
-#        sudo /home/femu/dummy 999 999
+#        sudo /home/sungjin/dummy 1 1
+#        sudo /home/sungjin/dummy 999 999
     done
 
     if [ $FAILED -eq 0 ]; then
@@ -205,5 +210,5 @@ do
     fi
 done
 
-sudo /home/femu/dummy2 111 111
+sudo /home/sungjin/dummy2 111 111
 sudo ${RESULT_DIR_ROOT_PATH}/sendresultmail
