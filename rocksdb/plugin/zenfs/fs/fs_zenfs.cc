9fe9ba3f7b3578df9f51918dbd31b146d737ffe6
gs201 <sg20180546@gmail.com>
2024-02-03 17:51:30 +0900

    measurePageCacheHit

--- rocksdb/plugin/zenfs/fs/fs_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/fs_zenfs.cc
@@ -2973,7 +2973,7 @@ IOStatus ZenFS::SMRLargeIOMigrateExtents(const std::vector<ZoneExtentSnapshot*>&
   // char* page_cache_check_hit_buffer_ = nullptr;
     if(page_cache_hit_mmap_addr_==nullptr){
       uint64_t device_total_size= 1<<30;
-      device_total_size =<<log2_DEVICE_IO_CAPACITY;
+      device_total_size <<=log2_DEVICE_IO_CAPACITY;
       page_cache_hit_mmap_addr_ = 
       (char*)mmap(NULL, device_total_size, PROT_READ, MAP_SHARED, read_fd, io_zone_start_offset);
     }
