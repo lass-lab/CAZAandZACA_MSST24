ddef16e032287a2ded4067e69fc2f59b278e9277
gs201 <sg20180546@gmail.com>
2024-02-12 15:33:40 +0900

    zcanalysis

--- rocksdb/plugin/zenfs/fs/fs_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/fs_zenfs.cc
@@ -735,7 +735,9 @@ void ZenFS::AsyncZoneCleaning(void){
       long elapsed_ns_timespec = (end_timespec.tv_sec - start_timespec.tv_sec) * 1000000000 + (end_timespec.tv_nsec - start_timespec.tv_nsec);
       long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
       (void)(microseconds);
-      (void)(elapsed_ns_timespec,end,start);
+      (void)(elapsed_ns_timespec);
+      (void)(end);
+      (void)(start);
       // zbd_->AddZCTimeLapse(start, end,(elapsed_ns_timespec/1000),
       //                     migrate_zones_start.size(),should_be_copied, false);
     }
