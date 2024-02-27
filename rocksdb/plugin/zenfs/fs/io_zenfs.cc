1899601db5d04b67020aa18ea8e8def7bd81407e
gs201 <sg20180546@gmail.com>
2024-02-27 22:29:34 +0900

    nostdmove

--- rocksdb/plugin/zenfs/fs/io_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/io_zenfs.cc
@@ -527,7 +527,7 @@ IOStatus ZoneFile::PositionedRead(uint64_t offset, size_t n, Slice* result,
     
 
     
-    // std::shared_ptr<char> page_cache = std::move(extent->page_cache_);
+    std::shared_ptr<char> page_cache = (extent->page_cache_);
     {
       std::lock_guard<std::mutex> lg(extent->page_cache_lock_);
       // std::shared_ptr<char> page_cache = (extent->page_cache_);
