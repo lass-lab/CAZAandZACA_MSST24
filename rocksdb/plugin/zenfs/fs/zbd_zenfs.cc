256c5d05e262e725f889f0fac2d9cb1801eede7c
gs201 <sg20180546@gmail.com>
2024-03-11 20:35:51 +0900

    openprior,notifyone,ifprioritizedreturn,nopagecache

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
@@ -2188,7 +2188,7 @@ void ZonedBlockDevice::WaitForOpenIOZoneToken(bool prioritized,WaitForOpenZoneCl
           zone_resources_priority_queue_.pop();
           return true;
         } else {
-          zone_resources_.notify_all();
+          zone_resources_.notify_one();
           return false;
         }
       });
@@ -2276,7 +2276,7 @@ void ZonedBlockDevice::PutOpenIOZoneToken() {
       open_io_zones_--;
     }
   }
-  zone_resources_.notify_all();
+  zone_resources_.notify_one();
 }
 
 void ZonedBlockDevice::PutActiveIOZoneToken() {
@@ -2286,7 +2286,7 @@ void ZonedBlockDevice::PutActiveIOZoneToken() {
       active_io_zones_--;
     }
   }
-  zone_resources_.notify_all();
+  zone_resources_.notify_one();
 }
 
 IOStatus ZonedBlockDevice::ApplyFinishThreshold() {
