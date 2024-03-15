ddf8ac16417ee43417cb032c2b93290ca26d1e4b
gs201 <sg20180546@gmail.com>
2024-03-15 17:35:08 +0900

    saturationpoint10

--- rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
+++ rocksdb/plugin/zenfs/fs/zbd_zenfs.cc
@@ -2351,16 +2351,16 @@ void ZonedBlockDevice::PutOpenIOZoneToken(WaitForOpenZoneClass open_class) {
     // }
   }
   if(AsyncZCEnabled()){
-    priority_zone_resources_[WAL].notify_one();
-    priority_zone_resources_[ZC].notify_one();
-    // if l0-l1 compaction blocked by l1-l2 compaction, scehdule l1-l2 aggresively.
-    double l0score = PredictCompactionScore(0);
-    double l1score = PredictCompactionScore(1);
+    // priority_zone_resources_[WAL].notify_one();
+    // priority_zone_resources_[ZC].notify_one();
+    // // if l0-l1 compaction blocked by l1-l2 compaction, scehdule l1-l2 aggresively.
+    // double l0score = PredictCompactionScore(0);
+    // double l1score = PredictCompactionScore(1);
 
-    if(l0score>1.0 && l1score>1.0 && l1score>l0score ){
-      priority_zone_resources_[L2].notify_one();
-    }
-    for(int oc =L0 ; oc< 10; oc++){
+    // if(l0score>1.0 && l1score>1.0 && l1score>l0score ){
+    //   priority_zone_resources_[L2].notify_one();
+    // }
+    for(int oc =0 ; oc< 10; oc++){
       // if(open_io_zones_.load()==max_nr_open_io_zones_ && oc>L2){
       //   break;
       // }
